275ed20a9ff5816951a8b93ec0bcfa33a0c635fc
RobisonTorres <robison.torres21@gmail.com>
2025-04-16 21:02:04 -0300

    converted json filo into hashmap.

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,26 @@
+use std::fs::File;
+use std::io::BufReader;
 use std::collections::HashMap;
-// Option
-/*
-struct Product {
-    price: f64,
-    brand: String,
-}
-*/
-fn main() {
+use serde_json::Result;
 
-    //let mut product_list: HashMap<String, HashMap<String, String>> = HashMap::new();
-    let mut product_list: HashMap<String, [String; 2]> = HashMap::new();
+fn main() -> Result<()> {
+    let file = File::open("products.json").expect("Failed to open file");
+    let reader = BufReader::new(file);
 
-    product_list.insert(String::from("Mouse"), 
-    ["Price: 19.99".to_string(), "Brand: Toyota".to_string()]);
+    type ProductDetails = (f64, String);
+    type ProductMap = HashMap<String, ProductDetails>;
+    type CategoryMap = HashMap<String, ProductMap>;
 
-    let item = String::from("Mouse");
+    let products: Vec<CategoryMap> = serde_json::from_reader(reader)?;
 
-    if let Some(info) = product_list.get(&item) {
-        println!("{item} - {} - {}", info[0], info[1]);
-    } else {
-            println!("Item '{}' not found.", item);
+    for category_map in products {
+        for (category, items) in category_map {
+            println!("Category: {}", category);
+            for (product, (price, brand)) in items {
+                println!("  - {}: ${:.2} ({})", product, price, brand);
+            }
+        }
     }
+
+    Ok(())
 }
-
-/*
-// Representação do grafo usando HashMap
-type Graph = HashMap<i32, Vec<i32>>;
-
-fn main() {
-    let mut grafo: Graph = HashMap::new();
-
-    // Adicionar nós (chaves)
-    grafo.insert(1, vec![]);
-    grafo.insert(2, vec![]);
-    grafo.insert(3, vec![]);
-
-    // Adicionar arestas (vizinhos)
-    grafo.get_mut(&1).unwrap().push(2);
-    grafo.get_mut(&2).unwrap().push(1);
-    grafo.get_mut(&2).unwrap().push(3);
-    grafo.get_mut(&3).unwrap().push(2);
-
-    // Percorrer o grafo e imprimir os vizinhos
-    for (no, vizinhos) in &grafo {
-        println!("No {}: Vizinhos {:?}", no, vizinhos);
-    }
-}
-*/

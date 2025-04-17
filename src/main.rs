c62433f9675901785d826e44e3a261b432b3d704
RobisonTorres <robison.torres21@gmail.com>
2025-04-17 18:20:20 -0300

    implemented struct and recommendation.

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,29 @@ use std::io::BufReader;
 use std::collections::HashMap;
 use serde_json::Result;
 
+#[derive(serde::Deserialize)]
+struct Product {
+    price: f64,
+    brand: String,
+    recommendation: Vec<String>,
+}
+
 fn main() -> Result<()> {
+
     let file = File::open("products.json").expect("Failed to open file");
     let reader = BufReader::new(file);
+    let mut product_list: HashMap<String, Product> = serde_json::from_reader(reader)?; 
 
-    type ProductDetails = (f64, String);
-    type ProductMap = HashMap<String, ProductDetails>;
-    type CategoryMap = HashMap<String, ProductMap>;
-
-    let products: Vec<CategoryMap> = serde_json::from_reader(reader)?;
-
-    for category_map in products {
-        for (category, items) in category_map {
-            println!("Category: {}", category);
-            for (product, (price, brand)) in items {
-                println!("  - {}: ${:.2} ({})", product, price, brand);
-            }
+    let item = String::from("Smartphone");
+    product_list.get_mut(&item).unwrap().recommendation.push("Mouse".to_string());
+    if let Some(info) = product_list.get(&item) {
+        println!("{} - ${:.2} - {}.", item, info.price, info.brand);
+        println!("Products relates");
+        for p in info.recommendation.iter() {
+            print!("{p} ")
         }
+    } else {
+        println!("Item '{}' not found.", item);
     }
-
     Ok(())
 }

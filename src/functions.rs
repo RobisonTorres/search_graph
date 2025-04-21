84f81b4a59044ebdbfeae98158b13e519a208884
RobisonTorres <robison.torres21@gmail.com>
2025-04-21 10:08:24 -0300

    Enhanced the search by info function.

--- src/functions.rs
+++ src/functions.rs
@@ -11,36 +11,35 @@ pub struct Product {
     pub recommendation: HashSet<String>,
 }
 
-pub fn read_json() -> HashMap<String, Product> {
+fn read_json() -> HashMap<String, Product> {
 
-    // This function...
+    // This function reads a JSON file and returns a HashMap of product names to Product structs.
     let file = File::open("products.json").expect("Failed to open file");
     let reader = BufReader::new(file);  
     let product_list: HashMap<String, Product> = serde_json::from_reader(reader).expect("REASON"); 
     product_list
 }
 
-pub fn search_product_by_name(input_user: String) -> bool {
+fn search_product_by_name(input_user: String) -> bool {
 
-    // This function...
+    // This function try to find a product by its exact name and prints its details if found.
     let product_list = read_json();
     if let Some(product) = product_list.get(&input_user) {
         println!("{} - ${:.2} - {}.", input_user, product.price, product.brand);
-        println!("Products relates");
+        println!("Products related");
         for i in product.recommendation.iter() {
             print!("{i} ")
         }
         true
     } else {
-        println!("{} product not found", input_user);
+        println!("Products related to your search {}.", input_user);
         false
     }    
 }    
 
-pub fn search_product_by_info(input_user: String) {
+/*fn search_product_by_info(input_user: String) {
 
-    // This function...
-    println!("Products related to your search {}.", input_user);
+    // This function searches for products based on brand or category and prints matching product names.
     let product_list = read_json();
     let mut products_found = vec![];
     for p in product_list.keys() {
@@ -57,16 +56,25 @@ pub fn search_product_by_info(input_user: String) {
         }
     }
     
+}*/
+
+fn search_product_by_info(input_user: String) {
+
+    // This function searches for products based on brand or category and prints matching product names.
+    let product_list = read_json();
+    for p in product_list.keys() {
+        let product = product_list.get(p);
+        if input_user == product.unwrap().brand || input_user == product.unwrap().category { 
+            println!("{} ", p);
+        }
+    }   
 }
 
 pub fn search_module(input_user: String) {
 
-    let product_list = read_json();
-    let test = input_user;
-    if product_list.contains_key(&test) {
-        search_product_by_name(test);
-    } else {
-        search_product_by_info(test);
+    // This function attempts to find a product by its name. If not found, searches by brand or category.
+    if !search_product_by_name(input_user.clone()) {
+        search_product_by_info(input_user);
     }
 }
 /*

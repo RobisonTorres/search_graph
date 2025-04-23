bd2fd15a75ac77bf83b4a3767d0a7372facb6d78
RobisonTorres <robison.torres21@gmail.com>
2025-04-23 19:09:11 -0300

    Refactored structures and JSON parsing to handle nodes and edges separately for graph representation.

--- src/functions.rs
+++ src/functions.rs
@@ -2,16 +2,26 @@ use std::io;
 use ucfirst::ucfirst;
 use std::fs::File;
 use std::io::BufReader;
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use strsim::levenshtein;
 
-#[derive(serde::Deserialize)]
-pub struct Product {  // For safety, use getters and setter here.
+#[derive(Debug, Deserialize)]
+pub struct Product {
     pub price: f64,
     pub brand: String,
     pub category: String,
-    pub recommendation: HashSet<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TopLevel {
+
+    #[serde(rename = "Product")]
+    pub product: HashMap<String, Product>,
+
+    #[serde(rename = "Recommendation")]
+    pub recommendation: HashMap<String, HashSet<String>>,
 }
 
 pub fn take_input_user() -> String {
@@ -19,36 +29,49 @@ pub fn take_input_user() -> String {
     // This function takes user's input.
     println!("Search for product...");
     let mut input: String = String::new();
-    io::stdin() 
-        .read_line(&mut input) 
+    io::stdin().read_line(&mut input) 
         .expect("Unable to read Stdin");
     let input = ucfirst(input.trim());
     input.to_string()
 }
 
-fn read_json() -> HashMap<String, Product> {
+pub fn read_json_products() -> HashMap<String, Product> {
 
-    // This function reads a JSON file and returns a HashMap of product names to Product structs.
+    // This function...
     let file = File::open("products.json").expect("Failed to open file");
-    let reader = BufReader::new(file);  
-    let product_list: HashMap<String, Product> = serde_json::from_reader(reader).expect("REASON"); 
-    product_list
+    let reader = BufReader::new(file);
+    let data: TopLevel = serde_json::from_reader(reader).expect("Failed to parse JSON");
+    data.product
+}
+
+pub fn read_json_recommendations() -> HashMap<String, HashSet<String>> {
+
+    // This function...
+    let file = File::open("products.json").expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let data: TopLevel = serde_json::from_reader(reader).expect("Failed to parse JSON");
+    data.recommendation
 }
 
 fn search_product_by_name(input_user: String) -> bool {
 
     // This function try to find a product by its exact name and prints its details if found.
-    let product_list: HashMap<String, Product> = read_json();
+    let product_list: HashMap<String, Product> = read_json_products();
+    let recommendation_list: HashMap<String, HashSet<String>> = read_json_recommendations();
     if let Some(product) = product_list.get(&input_user) {
-        println!("Product Found: ");
         println!("-----------------------------------");
+        println!("Product Found: \n");
         println!("Name: {}", input_user);
         println!("Price: ${:.2}", product.price);
         println!("Brand: {}", product.brand);
         print!("\nSee also: ");
-        for r in product.recommendation.iter() {
-            print!("{r} ")
-        }
+        if let Some(recommendations) = recommendation_list.get(&input_user) {
+            for r in recommendations {
+                print!("{} ", r);
+            }
+        } else {
+            println!("No recommendations found for {}", input_user);
+        }      
         println!("\n-----------------------------------");
         true
     } else { 
@@ -61,7 +84,7 @@ fn search_product_by_name(input_user: String) -> bool {
 fn search_product_by_info(input_user: String) {
 
     // This function searches for products based on brand or category and prints matching product names.
-    let product_list = read_json();
+    let product_list = read_json_products();
     let mut found = false;
     for product_find in product_list.keys() {
         let product = product_list.get(product_find);
@@ -73,6 +96,7 @@ fn search_product_by_info(input_user: String) {
     if !found {
         let spell_check = suggest_correction(input_user);
         println!("Did you mean {}?", spell_check);
+        search_product_by_name(spell_check);
     }   
     println!("-----------------------------------");
 }
@@ -88,12 +112,9 @@ pub fn search_module(input_user: String) {
 fn suggest_correction(misspelled: String) -> String {
     
     // This function finds the correct word based on the minimum Levenshtein distance.
-    let product_list: HashMap<String, Product> = read_json();
+    let product_list: HashMap<String, Product> = read_json_products();
     let dictionary = product_list.keys();
     dictionary
         .min_by_key(|word| levenshtein(&misspelled, word))
         .unwrap_or(&misspelled).to_string()
 }
-/*
-product_list.get_mut(&item).unwrap().recommendation.insert("Mouse".to_string());
-*/
